1ef83c6d1ef8c58c5cf3b2638f5cf43758772b5e
Tan Wei Yang <weiyang.tan@pixelstack.my>
2020-12-05 14:54:29 +0800

    Generate Files

--- extension.js
+++ extension.js
@@ -22,6 +22,73 @@ function activate(context) {
 
 		let upperCaseName = name[0].toUpperCase() + name.substring(1);
 
+		let useCasesFolder = path.join(choosenPath, `use_cases`);
+		let entityFolder = path.join(choosenPath, `entity`);
+		let controllerFolder = path.join(choosenPath, `controller`);
+
+		let useCasesFolderPath = path.join(useCasesFolder, `${name}`);
+		let entityFolderPath = path.join(entityFolder, `${name}`);
+		let controllerFolderPath = path.join(controllerFolder, `${name}`);
+
+		let errorHandler = (err) => {
+			if(err){
+				console.error(err);
+				return vscode.window.showErrorMessage("Failed to genrate folder structure");
+			}
+		}
+
+		let createEntityFiles = () => {
+			let entityFile = path.join(entityFolderPath, `${name}.js`);
+			let entityIndexFile = path.join(entityFolderPath, `index.js`);
+
+			let entityContent = `
+export default function buildMake${upperCaseName} ({}) {
+	return function make${upperCaseName}
+}`;
+			let entityIndexContent = `
+const make${upperCaseName} = buildMake${upperCaseName}({});
+
+export default make${upperCaseName};`;
+
+			fs.writeFile(entityFile, entityContent, errorHandler);
+			fs.writeFile(entityIndexFile, entityIndexContent, errorHandler);
+		}
+
+		let createUseCasesFiles = () => {
+			let useCasesIndexFile = path.join(useCasesFolderPath, `index.js`);
+
+			let useCasesContent = `
+const ${name}Service = Object.freeze({});
+
+export default ${name}Service;`;
+
+			fs.writeFile(useCasesIndexFile, useCasesContent, errorHandler);
+		}
+
+		let createControllerFiles = () => {
+			let controllerIndexFile = path.join(controllerFolderPath, `index.js`);
+
+			let controllerIndexContent = `
+import {} from "../use_cases/${name}"
+
+const ${name}Controller = Object.freeze({});
+
+export default ${name}Controller;`;
+
+			fs.writeFile(controllerIndexFile, controllerIndexContent, errorHandler);
+		}
+
+		if(!fs.existsSync(useCasesFolderPath)){
+			fs.mkdir(useCasesFolderPath, {recursive : true}, createUseCasesFiles);
+		}
+
+		if(!fs.existsSync(entityFolderPath)){
+			fs.mkdir(entityFolderPath, {recursive : true}, createEntityFiles);
+		}
+		
+		if(!fs.existsSync(controllerFolderPath)){
+			fs.mkdir(controllerFolderPath, {recursive : true}, createControllerFiles);
+		}
 
 		vscode.window.showInformationMessage('Hello World from NodeJS Clean Code Generator!');
 	});
